31030713c41620c35eb452ea50261367da0d7d2e
joaoolucas <joaolucasliberato4@gmail.com>
2025-01-07 10:44:17 -0300

    Use existing favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,17 +34,7 @@ export const metadata: Metadata = {
     images: ['/og-image.png'],
   },
   icons: {
-    icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
-    ],
-    apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
-    ],
-    other: [
-      { rel: 'mask-icon', url: '/safari-pinned-tab.svg', color: '#ff6b00' }
-    ]
+    icon: '/favicon.ico'
   },
 }
 
